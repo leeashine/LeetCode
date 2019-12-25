c83880349b63ec5b8cdfa3d36e48d6ea5afc2ff4
935391770@qq.com <935391770@qq.com>
2019-12-25 16:19:36 +0800

    队列问题

--- src/main/java/aleetcode/Contest168.java
+++ src/main/java/aleetcode/Contest168.java
@@ -1,8 +1,6 @@
 package aleetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Contest168 {
 
@@ -13,7 +11,47 @@ public class Contest168 {
 //        System.out.println(b);
         new Contest168().maxFreq2("aababcaab", 2, 3, 4);
 
+        int [] status={1,0,0,0,0,0},candies = {1,1,1,1,1,1},initialBoxes = {0};
+        int [][] keys = {{1,2,3,4,5},{},{},{},{},{}}, containedBoxes = {{1,2,3,4,5},{},{},{},{},{}};
+
+        maxCandies(status,candies,keys,containedBoxes,initialBoxes);
     }
+
+//    Input: status = [1,0,1,0], candies = [7,5,4,100], keys = [[],[],[1],[]], containedBoxes = [[1,2],[3],[],[]], initialBoxes = [0]
+//    Output: 16
+//    Explanation: You will be initially given box 0. You will find 7 candies in it and boxes 1 and 2. Box 1 is closed and you don't have a key for it so you will open box 2. You will find 4 candies and a key to box 1 in box 2.
+//    In box 1, you will find 5 candies and box 3 but you will not find a key to box 3 so box 3 will remain closed.
+//    Total number of candies collected = 7 + 4 + 5 = 16 candy.
+    public static int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+
+        int n = status.length; // count of boxes
+        boolean[] usedBoxes = new boolean[n]; // this are used once
+        boolean[] boxFound = new boolean[n];// new box we found
+        Queue<Integer> q = new LinkedList<>();
+        for (int v : initialBoxes) {
+            q.add(v);
+            boxFound[v] = true; // initial boxes
+        }
+        int candy = 0;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (status[cur] == 1 && !usedBoxes[cur]) { // not used and box open
+                candy += candies[cur];
+                usedBoxes[cur] = true;
+                for (int k : keys[cur]) { // all keys in that box
+                    status[k] = 1;
+                    if (boxFound[k]) q.add(k);// box was found and we have the key
+                }
+                for (int k : containedBoxes[cur]) {// all boxes in cur box
+                    boxFound[k] = true;
+                    q.add(k);
+                }
+            }
+        }
+        return candy;
+
+    }
+
 
     //    Input: s = "aababcaab", maxLetters = 2, minSize = 3, maxSize = 4
 //    Output: 2
@@ -24,21 +62,26 @@ public class Contest168 {
         int l=0,r=0,letter=0;
         int [] ch=new int [26];
         while(r<s.length()){
-            if(ch[ s.charAt(r) -'a' ]==0) letter++;
-            r++;
-            ch[s.charAt(r)-'a']++;
+            if(ch[ s.charAt(r)-'a']==0){
 
-            while( letter>maxLetters|| (r-l)>minSize ){
-                if(ch[s.charAt(l)-'a']==1) letter--;
-                l++;
-                ch[s.charAt(l)-'a']--;
+                letter++;
+
             }
-            if((r-l)==minSize){
+            ch[ s.charAt(r)-'a']++;
+            r++;
+            while(letter>maxLetters||(r-l)>minSize){
+                if(ch[ s.charAt(l)-'a']==1){
+
+                    letter--;
+                }
+                ch[ s.charAt(l)-'a']--;
+                l++;
+            }
+            if((r-l)== minSize){
                 String sb=s.substring(l,r);
                 map.put(sb,map.getOrDefault(sb,0)+1);
                 res=Math.max(res,map.get(sb));
             }
-
         }
 
         return res;
