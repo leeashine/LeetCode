67b551dcb0e09c7675d64575e69664cfff6af78f
935391770@qq.com <935391770@qq.com>
2019-12-26 10:12:50 +0800

    加深queue题

--- src/main/java/aleetcode/Contest168.java
+++ src/main/java/aleetcode/Contest168.java
@@ -24,27 +24,27 @@ public class Contest168 {
 //    Total number of candies collected = 7 + 4 + 5 = 16 candy.
     public static int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
 
-        int n = status.length; // count of boxes
-        boolean[] usedBoxes = new boolean[n]; // this are used once
-        boolean[] boxFound = new boolean[n];// new box we found
-        Queue<Integer> q = new LinkedList<>();
-        for (int v : initialBoxes) {
-            q.add(v);
-            boxFound[v] = true; // initial boxes
+        int n=status.length;
+        boolean [] usedBoxes=new boolean[n];
+        boolean [] boxFound=new  boolean[n];
+        Queue<Integer> q=new LinkedList();
+        for(int i:initialBoxes){
+            q.add(i);
+            boxFound[i]=true;
         }
-        int candy = 0;
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            if (status[cur] == 1 && !usedBoxes[cur]) { // not used and box open
-                candy += candies[cur];
-                usedBoxes[cur] = true;
-                for (int k : keys[cur]) { // all keys in that box
-                    status[k] = 1;
-                    if (boxFound[k]) q.add(k);// box was found and we have the key
+        int candy=0;
+        while (!q.isEmpty()){
+            int cur=q.poll();
+            if(status[cur]==1&&!usedBoxes[cur]){//有钥匙并且箱子没有打开过
+                candy+=candies[cur];
+                usedBoxes[cur]=true;
+                for(int i:keys[cur]){
+                    status[i]=1;
+                    if(boxFound[i]) q.add(i);
                 }
-                for (int k : containedBoxes[cur]) {// all boxes in cur box
-                    boxFound[k] = true;
-                    q.add(k);
+                for(int i:containedBoxes[cur]){
+                    boxFound[i]=true;
+                    q.add(i);
                 }
             }
         }
