c14918d85a4a4a89f5663f0cab5fba7a9c7f8e4c
leeashine <935391770@qq.com>
2020-02-08 11:57:07 +0800

    1.8 Stram流操作的File.lines()

--- src/main/java/java8/chap5/BuildingStreams.java
+++ src/main/java/java8/chap5/BuildingStreams.java
@@ -11,8 +11,8 @@ import java.util.stream.Stream;
 
 public class BuildingStreams {
 
-    public static void main(String...args) throws Exception{
-        
+    public static void main(String... args) throws Exception {
+
         // Stream.of
         Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
         stream.map(String::toUpperCase).forEach(System.out::println);
@@ -26,48 +26,49 @@ public class BuildingStreams {
 
         // Stream.iterate
         Stream.iterate(0, n -> n + 2)
-              .limit(10)
-              .forEach(System.out::println);
+                .limit(10)
+                .forEach(System.out::println);
 
         // fibonnaci with iterate
-        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1],t[0] + t[1]})
-              .limit(10)
-              .forEach(t -> System.out.println("(" + t[0] + ", " + t[1] + ")"));
-        
-        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1],t[0] + t[1]})
-              .limit(10)
-              . map(t -> t[0])  //使用Map提取每个元组中的第一个元素
-              .forEach(System.out::println);
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .forEach(t -> System.out.println("(" + t[0] + ", " + t[1] + ")"));
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .map(t -> t[0])  //使用Map提取每个元组中的第一个元素
+                .forEach(System.out::println);
 
         // random stream of doubles with Stream.generate
         Stream.generate(Math::random)
-              .limit(10)
-              .forEach(System.out::println);
- 
+                .limit(10)
+                .forEach(System.out::println);
+
         // stream of 1s with Stream.generate
         IntStream.generate(() -> 1)
-                 .limit(5)
-                 .forEach(System.out::println);
+                .limit(5)
+                .forEach(System.out::println);
 
-        IntStream.generate(new IntSupplier(){
-            public int getAsInt(){
+        IntStream.generate(new IntSupplier() {
+            public int getAsInt() {
                 return 2;
             }
         }).limit(5)
-          .forEach(System.out::println);
-   
+                .forEach(System.out::println);
 
-        IntSupplier fib = new IntSupplier(){
-                  private int previous = 0;
-                  private int current = 1;
-                  public int getAsInt(){
-                      int nextValue = this.previous + this.current;
-                      this.previous = this.current;
-                      this.current = nextValue;
-                      return this.previous;
-                  }
-              };
-         IntStream.generate(fib).limit(10).forEach(System.out::println);
+
+        IntSupplier fib = new IntSupplier() {
+            private int previous = 0;
+            private int current = 1;
+
+            public int getAsInt() {
+                int nextValue = this.previous + this.current;
+                this.previous = this.current;
+                this.current = nextValue;
+                return this.previous;
+            }
+        };
+        IntStream.generate(fib).limit(10).forEach(System.out::println);
 
 //         Files.lines，它会返回一个由指定文件中的各行构成的字符串流。
 //         流会自动关闭
@@ -76,16 +77,16 @@ public class BuildingStreams {
 //平的单词流，而不是给每一行生成一个单词流。最后，把distinct和count方法链接起来，数
 //数流中有多少各不相同的单词。
 //        flatmap方法让你把一个流中的每个值都换成另一个流，然后把所有的流连接起来成为一个流。
-         long uniqueWords = 0;
-         try(Stream<String> lines =Files.lines(Paths.get("D:\\workspace\\aleetcode\\src\\main\\java\\java8\\chap5\\data.txt"),Charset.defaultCharset())) {
-             uniqueWords=lines.flatMap(line->Arrays.stream(line.split(" ")))
-                     .distinct()
-                     .count();
-         }catch (IOException e){
-             e.printStackTrace();
-         }
+        long uniqueWords = 0;
+        try (Stream<String> lines = Files.lines(Paths.get("D:\\workspace\\aleetcode\\src\\main\\java\\java8\\chap5\\data.txt"), Charset.defaultCharset())) {
+            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
+                    .distinct()
+                    .count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-         System.out.println("There are " + uniqueWords + " unique words in data.txt");
+        System.out.println("There are " + uniqueWords + " unique words in data.txt");
 
 
     }
