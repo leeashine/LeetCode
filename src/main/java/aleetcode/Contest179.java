06418bd14ebabb1b6286c5f3401f68def92366e4
leeashine <935391770@qq.com>
2020-03-15 09:40:38 +0800

    最长上升子序列的长度

--- src/main/java/aleetcode/Contest179.java
+++ src/main/java/aleetcode/Contest179.java
@@ -1,5 +1,8 @@
 package aleetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Contest179 {
 
 
@@ -8,27 +11,33 @@ public class Contest179 {
 //        String s = generateTheString(4);
 //        System.out.println(s);
 
-        int []manager ={1,2,3,4,5,6,-1};
-        int []informTime ={0,6,5,4,3,2,1};
-        int i = numOfMinutes(7,6,manager ,informTime);
-        System.out.println(i);
+//        int []manager ={1,2,3,4,5,6,-1};
+//        int []informTime ={0,6,5,4,3,2,1};
+//        int i = numOfMinutes(7,6,manager ,informTime);
+//        System.out.println(i);
+
+//        canThreePartsEqualSum(new int[]{10,-10,10,-10,10,-10,10,-10});
 
 
     }
+
+
     //领导通知时间(递归)
-    private static int []count;
-    private static int sum=0;
+    private static int[] count;
+    private static int sum = 0;
+
     public static int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        count=new int[n];
-        findNext(manager,headID,informTime);
+        count = new int[n];
+        findNext(manager, headID, informTime);
         return sum;
     }
-    public static void findNext(int[] par, int dest,int []informTime) {
-        for(int i=0;i<par.length;i++){
-            if(par[i]==dest&&count[par[i]]!=1){
-                count[par[i]]=1;
-                sum+=informTime[dest];
-                findNext(par,i,informTime);
+
+    public static void findNext(int[] par, int dest, int[] informTime) {
+        for (int i = 0; i < par.length; i++) {
+            if (par[i] == dest && count[par[i]] != 1) {
+                count[par[i]] = 1;
+                sum += informTime[dest];
+                findNext(par, i, informTime);
             }
         }
     }
@@ -37,21 +46,21 @@ public class Contest179 {
     //灯泡 当前位置及左边全开才变蓝
     public static int numTimesAllBlue(int[] light) {
 
-        int res=0;
-        int [] count=new int[light.length];
-        int max=Integer.MIN_VALUE;
+        int res = 0;
+        int[] count = new int[light.length];
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < light.length; i++) {
 
-            int idx=light[i];
-            count[idx-1]=1;
-            int ment=0;
-            max=Math.max(max,idx);
-            for(int j=0;j<max;j++){
-                if(count[j]==1)
+            int idx = light[i];
+            count[idx - 1] = 1;
+            int ment = 0;
+            max = Math.max(max, idx);
+            for (int j = 0; j < max; j++) {
+                if (count[j] == 1)
                     ment++;
                 else break;
             }
-            if(ment==(max))
+            if (ment == (max))
                 res++;
 
         }
@@ -61,18 +70,18 @@ public class Contest179 {
 
     public static String generateTheString(int n) {
 
-        String res="";
-        String c= "a";
-        if(n%2==0){
+        String res = "";
+        String c = "a";
+        if (n % 2 == 0) {
 
-            for(int i=0;i<n-1;i++){
-                res+=c;
+            for (int i = 0; i < n - 1; i++) {
+                res += c;
             }
-            res+="b";
+            res += "b";
 
-        }else{
-            for(int i=0;i<n;i++){
-                res+=c;
+        } else {
+            for (int i = 0; i < n; i++) {
+                res += c;
             }
 
         }
--- src/main/java/aleetcode/DailyOneProblem.java
+++ src/main/java/aleetcode/DailyOneProblem.java
@@ -1,9 +1,6 @@
 package aleetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class DailyOneProblem {
 
@@ -14,48 +11,134 @@ public class DailyOneProblem {
 //        int i = coinChange(coins, amount);
 //        System.out.println(i);
 
+//        int gcd = gcd(10, 8);
+////        System.out.println(gcd);
+//
+//        int i = lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18});
+//        System.out.println(i);
+
     }
 
+    //    最长上升子序列的长度。
+    public static int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
+    //多数元素
+    public int majorityElement(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+
+    }
+
+    //字符串的最大公因子
+//    输入：str1 = "ABCABC", str2 = "ABC"
+//    输出："ABC"
+    public String gcdOfStrings(String str1, String str2) {
+        // 假设str1是N个x，str2是M个x，那么str1+str2肯定是等于str2+str1的。
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        // 辗转相除法求gcd。
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+
+    //求最大公共除数
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+
+    //以数数组分成和相等的三份（连续）
+    public static boolean canThreePartsEqualSum(int[] A) {
+        int[] prefixSum = new int[A.length];
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            prefixSum[i] = sum;
+        }
+        if (sum % 3 == 0) {
+            int pre = sum / 3;
+            int count = 0;
+            for (int i = 0; i < prefixSum.length && count < 3; i++) {
+                if (pre == prefixSum[i]) {
+                    count++;
+                    pre += sum / 3;
+                }
+
+            }
+            return count == 3 ? true : false;
+
+        } else {
+            return false;
+        }
+    }
 
     //求二叉树的最大直径 两个结点之间最大距离
 //    时间复杂度：O(N)O(N)，其中 NN 为二叉树的节点数，即遍历一棵二叉树的时间复杂度，每个结点只被访问一次。
 //    空间复杂度：O(Height)O(Height)，其中 HeightHeight 为二叉树的高度。由于递归函数在递归过程中需要为每一层递归函数分配栈空间，所以这里需要额外的空间且该空间取决于递归的深度，而递归的深度显然为二叉树的高度，并且每次递归调用的函数里又只用了常数个变量，所以所需空间复杂度为 O(Height)O(Height) 。
-    int res=1;
+    int res = 1;
 
     public int diameterOfBinaryTree(TreeNode root) {
 
         dfs(root);
-        return res-1;
+        return res - 1;
 
     }
 
-    public int dfs(TreeNode root){
-        if(root==null){
+    public int dfs(TreeNode root) {
+        if (root == null) {
             return 0;
         }
-        int left=0;
-        int right=0;
-        if(root.left!=null){
-            left=dfs(root.left);// 左儿子为根的子树的深度
+        int left = 0;
+        int right = 0;
+        if (root.left != null) {
+            left = dfs(root.left);// 左儿子为根的子树的深度
         }
-        if(root.right!=null){
-            right=dfs(root.right);// 右儿子为根的子树的深度
+        if (root.right != null) {
+            right = dfs(root.right);// 右儿子为根的子树的深度
         }
-        res=Math.max(left+right+1,res);
-        return Math.max(left,right)+1;// 返回该节点为根的子树的深度
+        res = Math.max(left + right + 1, res);
+        return Math.max(left, right) + 1;// 返回该节点为根的子树的深度
     }
 
     //买卖股票的最佳利润
     //求当前位置左边最小和最大值的差
     public int maxProfit(int[] prices) {
-        int max=0;
-        int min=Integer.MAX_VALUE;
+        int max = 0;
+        int min = Integer.MAX_VALUE;
         int price;
         for (int i = 0; i < prices.length; i++) {
-            price=prices[i];//2
-            min=Math.min(min,price);
-            if(price-min>max){
-                max=price-min;
+            price = prices[i];//2
+            min = Math.min(min, price);
+            if (price - min > max) {
+                max = price - min;
             }
         }
         return max;
