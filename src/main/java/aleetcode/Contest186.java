014784a7b92054ebeab0d2e9409548d24e43d87d
935391770@qq.com <935391770@qq.com>
2020-04-27 18:34:40 +0800

    LFU 最不经常使用缓存算法设计 程序设计 自定义一个双向链表+map

---
+++ src/main/java/aleetcode/Contest186.java
@@ -0,0 +1,28 @@
+package aleetcode;
+
+public class Contest186 {
+    public static void main(String[] args) {
+
+        String s = "011101";
+        maxScore(s);
+
+    }
+
+    //    「分割字符串的得分」为 左 子字符串中 0 的数量加上 右 子字符串中 1 的数量。
+    //     理解1：统计连续0的个数，直到碰到1停止
+//         理解2：类似动态规划，每次遍历都统计取最大值
+    public static int maxScore(String s) {
+        int res = 0, cnt1 = 0, cnt0 = 0;        //cnt1统计右边1的个数，同理cnt0左边0的个数
+        for (int i = 0; i < s.length(); i++) {
+            cnt1 += s.charAt(i) - '0';            //先统计1的个数
+        }                                       //由于左右区域的数至少为1，所以i不能等于len-1
+        for (int i = 0; i < s.length() - 1; i++) {  //点i分为左右两个区域
+            if (s.charAt(i) == '0') cnt0++;      //遇到01就统计，动态更新左右区域01个数
+            else cnt1--;
+            res = Math.max(res, cnt0 + cnt1);
+        }
+        return res;
+    }
+
+
+}
---
+++ src/main/java/aleetcode/LFUCache.java
@@ -0,0 +1,130 @@
+package aleetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 请你为 最不经常使用（LFU）缓存算法设计并实现数据结构。它应该支持以下操作：get 和 put。
+ *
+ *         get(key) - 如果键存在于缓存中，则获取键的值（总是正数），否则返回 -1。
+ *         put(key, value) - 如果键已存在，则变更其值；如果键不存在，请插入键值对。当缓存达到其容量时，则应该在插入新项之前，使最不经常使用的项无效。在此问题中，当存在平局（即两个或更多个键具有相同使用频率）时，应该去除最久未使用的键。
+ *         「项的使用次数」就是自插入该项以来对其调用 get 和 put 函数的次数之和。使用次数会在对应项被移除后置为 0 。
+ *
+ *          
+ *
+ *         进阶：
+ *         你是否可以在 O(1) 时间复杂度内执行两项操作？
+ */
+
+class LFUCache {
+    Map<Integer, Node> cache; // 存储缓存的内容
+    Map<Integer, DoublyLinkedList> freqMap; // 存储每个频次对应的双向链表
+    int size;
+    int capacity;
+    int min; // 存储当前最小频次
+
+    public LFUCache(int capacity) {
+        cache = new HashMap<>(capacity);
+        freqMap = new HashMap<>();
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        Node node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        freqInc(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (capacity == 0) {
+            return;
+        }
+        Node node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            freqInc(node);
+        } else {
+            if (size == capacity) {
+                DoublyLinkedList minFreqLinkedList = freqMap.get(min);
+                cache.remove(minFreqLinkedList.tail.pre.key);
+                minFreqLinkedList.removeNode(minFreqLinkedList.tail.pre); // 这里不需要维护min, 因为下面add了newNode后min肯定是1.
+                size--;
+            }
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            DoublyLinkedList linkedList = freqMap.get(1);
+            if (linkedList == null) {
+                linkedList = new DoublyLinkedList();
+                freqMap.put(1, linkedList);
+            }
+            linkedList.addNode(newNode);
+            size++;
+            min = 1;
+        }
+    }
+
+    void freqInc(Node node) {
+        // 从原freq对应的链表里移除, 并更新min
+        int freq = node.freq;
+        DoublyLinkedList linkedList = freqMap.get(freq);
+        linkedList.removeNode(node);
+        if (freq == min && linkedList.head.post == linkedList.tail) {
+            min = freq + 1;
+        }
+        // 加入新freq对应的链表
+        node.freq++;
+        linkedList = freqMap.get(freq + 1);
+        if (linkedList == null) {
+            linkedList = new DoublyLinkedList();
+            freqMap.put(freq + 1, linkedList);
+        }
+        linkedList.addNode(node);
+    }
+}
+
+class Node {
+    int key;
+    int value;
+    int freq = 1;
+    Node pre;
+    Node post;
+
+    public Node() {}
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+//自定义一个双向链表
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList() {
+        head = new Node();
+        tail = new Node();
+        head.post = tail;
+        tail.pre = head;
+    }
+
+    void removeNode(Node node) {
+        node.pre.post = node.post;
+        node.post.pre = node.pre;
+    }
+
+    void addNode(Node node) {
+        node.post = head.post;
+        head.post.pre = node;
+        head.post = node;
+        node.pre = head;
+    }
+}
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
