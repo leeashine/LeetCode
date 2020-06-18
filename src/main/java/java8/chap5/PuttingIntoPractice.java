610de54a5d0f445ebef6ccde2ba1749403096b80
935391770@qq.com <935391770@qq.com>
2020-06-18 15:16:36 +0800

    java8 流操作

--- src/main/java/java8/chap5/PuttingIntoPractice.java
+++ src/main/java/java8/chap5/PuttingIntoPractice.java
@@ -2,6 +2,10 @@ package java8.chap5;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
@@ -84,6 +88,77 @@ public class PuttingIntoPractice{
             transactions.stream()
                         .map(Transaction::getValue)
                         .reduce(0, Integer::max);
-        System.out.println(highestValue);      
+        System.out.println(highestValue);
+
+
+
     }
+
+    /**
+     * @param areaCode
+     */
+    private final Transaction findAreaOrg(String areaCode,List<Transaction> lists)
+    {
+        Stream<Transaction> stream = null;
+        try
+        {
+            stream = lists.stream();
+            Optional<Transaction> op = stream.filter(new Predicate<Transaction>() {
+                @Override
+                public boolean test(Transaction biz) {
+                    try {
+                        int value = biz.getValue();
+                        return areaCode.equals(value+"");
+                    } catch (Exception e) {
+//                        logger.warn(e.getMessage(),e);
+                        return false;
+                    }
+                }
+            }).findFirst();
+            Transaction resultBiz = op.isPresent()?op.get():null;
+            return resultBiz;
+        }
+        finally
+        {
+            try{if(stream != null) stream.close();}catch(Exception ex) {}
+        }
+    }
+
+    /**
+     * 	根据渠道代码进行代码转换，如无法转换，则使用OtherWish
+     * @param codeNo
+     * @param channelItemNo
+     */
+//    public String getChannelCodeMapItem(String codeNo,String channelItemNo)
+//    {
+//        MerchantCodeMapItem returnItem = null;
+//        Stream<MerchantCodeMapItem> stream = null;
+//        try
+//        {
+//            List<MerchantCodeMapItem> codeList = channelCodeMap.get(codeNo);
+//            if(codeList != null)
+//            {
+//                stream = codeList.stream();
+//                List<MerchantCodeMapItem> lists = stream
+//                        .filter((item) -> StringUtils.equals(channelItemNo, item.getChannelItemNo()))
+//                        .collect(Collectors.toList());
+//                if(lists.size()==0)
+//                {
+//                    if(channelCodeOtherWish.containsKey(codeNo))
+//                        return channelCodeOtherWish.get(codeNo).getItemNo();
+//                    else
+//                        return null;
+//                }
+//                else
+//                {
+//                    return lists.get(0).getItemNo();
+//                }
+//            }
+//            return null;
+//        }
+//        finally
+//        {
+//            if(stream != null) stream.close();
+//        }
+//    }
 }
