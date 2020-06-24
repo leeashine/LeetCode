f7e8288476b9a92b2bf67cd09ef66c53924e57c2
935391770@qq.com <935391770@qq.com>
2020-06-24 14:23:19 +0800

    并查集

---
+++ src/main/java/aleetcode/UnionFind.java
@@ -0,0 +1,41 @@
+package aleetcode;
+
+public class UnionFind {
+    final int[] parents;
+    int count;
+
+    public UnionFind(int n){
+        this.parents = new int[n];
+        reset();
+    }
+
+    public void reset(){
+        for(int i =0;i<parents.length;i++){
+            parents[i] = i;
+        }
+        count = parents.length;
+    }
+
+    public int find(int i){
+        int parent = parents[i];
+        if(parent == i){
+            return i;
+        }else{
+            int root = find(parent);
+            parents[i] = root;
+            return root;
+        }
+    }
+
+    public boolean union(int i, int j){
+        int r1 = find(i);
+        int r2 = find(j);
+        if(r1 != r2){
+            count--;
+            parents[r1] = r2;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
