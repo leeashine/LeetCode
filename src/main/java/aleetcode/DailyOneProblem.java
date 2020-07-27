cdf52f6b77ab625eba95265502127fce2b30c934
935391770@qq.com <935391770@qq.com>
2020-07-27 09:26:10 +0800

    贪心算法-----优势洗牌

--- src/main/java/aleetcode/DailyOneProblem.java
+++ src/main/java/aleetcode/DailyOneProblem.java
@@ -60,18 +60,51 @@ public class DailyOneProblem {
 //
 //        new DailyOneProblem().sortedArrayToBST(new int[]{-10,-3,0,5,9});
 
-        int[][] grid={{1,3,1},{1,5,1},{4,2,1}};
-        new DailyOneProblem().minPathSum(grid);
+//        int[][] grid={{1,3,1},{1,5,1},{4,2,1}};
+//        new DailyOneProblem().minPathSum(grid);
+
 
     }
+//    优势洗牌 贪心算法
+//    输入：A = [12,24,8,32], B = [13,25,32,11]
+//    输出：[24,32,8,12]
+    public int[] advantageCount(int[] A, int[] B) {
+        int[] sortedA = A.clone();
+        Arrays.sort(sortedA);
+        int[] sortedB = B.clone();
+        Arrays.sort(sortedB);
 
-    //除数博弈
-    public boolean divisorGame(int N) {
+        // assigned[b] = list of a that are assigned to beat b
+        Map<Integer, Deque<Integer>> assigned = new HashMap();
+        for (int b: B) assigned.put(b, new LinkedList());
+        // remaining = list of a that are not assigned to any b
 
+        Deque<Integer> remaining = new LinkedList();
 
-        return true;
+        // populate (assigned, remaining) appropriately
+        // sortedB[j] is always the smallest unassigned element in B
+        int j = 0;
+        for (int a: sortedA) {
+            if (a > sortedB[j]) {
+                assigned.get(sortedB[j++]).add(a);
+            } else {
+                remaining.add(a);
+            }
+        }
 
+        // Reconstruct the answer from annotations (assigned, remaining)
+        int[] ans = new int[B.length];
+        for (int i = 0; i < B.length; ++i) {
+            // if there is some a assigned to b...
+            if (assigned.get(B[i]).size() > 0)
+                ans[i] = assigned.get(B[i]).pop();
+            else
+                ans[i] = remaining.pop();
+        }
+        return ans;
     }
+
+
 
     //最小路径和
     //每次只能向下或者向右移动一步。
