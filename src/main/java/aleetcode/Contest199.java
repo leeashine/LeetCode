5c6fbf8a10d7c8a78fd26074bb90ced76192a5be
935391770@qq.com <935391770@qq.com>
2020-07-30 15:37:58 +0800

    >>>无符号右移 防止负数情况

--- src/main/java/aleetcode/Contest199.java
+++ src/main/java/aleetcode/Contest199.java
@@ -21,11 +21,11 @@ public class Contest199 {
     static final int tableSizeFor(int cap) {
         //对本身就是2的n次幂 减1进行处理
         int n = cap - 1;
-        n |= n >> 1;
-        n |= n >> 2;
-        n |= n >> 4;
-        n |= n >> 8;
-        n |= n >> 16;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
         return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
     }
 
