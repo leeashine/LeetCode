4e763ef944e2592ad48f1f5842a23a94c165ab88
leeashine <935391770@qq.com>
2020-08-12 20:56:32 +0800

    经典线程同步问题------消费者生产者

---
+++ src/main/java/juc/producerandconsume/ProducerAndConsumerService.java
@@ -0,0 +1,42 @@
+package juc.producerandconsume;
+
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ProducerAndConsumerService {
+    private ReentrantLock lock=new ReentrantLock();
+    private Condition condition=lock.newCondition();
+    private int sum=0;
+    private int MAX=10;
+    public void produce(){
+        try{
+            lock.lock();
+            while (sum==MAX){
+                condition.await();
+            }
+            System.out.println("生产消息");
+            sum++;
+            condition.signal();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+    public void consume(){
+        try{
+            lock.lock();
+            while (sum==0){
+                condition.await();
+            }
+            System.out.println("消费消息");
+            sum--;
+            condition.signal();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ src/main/java/juc/producerandconsume/Test.java
@@ -0,0 +1,25 @@
+package juc.producerandconsume;
+
+public class Test {
+    public static void main(String[] args) {
+        ProducerAndConsumerService service=new ProducerAndConsumerService();
+        new Thread(()->{
+            for (int i=0;i<20;i++) {
+                service.produce();
+            }
+        },"producerThread").start();
+        new Thread(()->{
+            for (int i=0;i<20;i++) {
+                service.consume();
+            }
+        },"consumerThread").start();
+//        ThreadProducer[] threadProducers=new ThreadProducer[100];
+//        ThreadConsumer[] threadConsumers=new ThreadConsumer[100];
+//        for(int i=0;i<100;i++){
+//            threadProducers[i]=new ThreadProducer(service);
+//            threadConsumers[i]=new ThreadConsumer(service);
+//            threadProducers[i].start();
+//            threadConsumers[i].start();
+//        }
+    }
+}
