3eb24f5b704bdb6c4f99bbc7c68ec8ece3faa436
Lee <935391770@qq.com>
2021-10-31 21:33:39 +0800

    guava 限流

--- src/main/java/iguava/Limiter.java
+++ src/main/java/iguava/Limiter.java
@@ -1,15 +1,15 @@
 package iguava;
 
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 import com.google.common.util.concurrent.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Limiter {
 
@@ -17,29 +17,23 @@ public class Limiter {
 
     private static final int CAPACITY_SIZE = 1000;
 
-    private long LIMIT_TIME = 1000;
+    private static final long LIMIT_TIME = 1000;
     /**
      * qps
      */
     private static final double limit = 5;
 
-    private Cache<String, RateLimiter> CACHE = CacheBuilder.newBuilder().initialCapacity(CAPACITY_SIZE)
+    private static final Cache<String, RateLimiter> CACHE = Caffeine.newBuilder().initialCapacity(CAPACITY_SIZE)
             .expireAfterAccess(1, TimeUnit.MINUTES)
             .build();
 
     public boolean isLimit(String accessKeyID) {
-        RateLimiter limiter = null;
-        try {
-            limiter = CACHE.get(accessKeyID, new Callable<RateLimiter>() {
-                @Override
-                public RateLimiter call() throws Exception {
-                    return RateLimiter.create(limit);
-                }
-            });
-
-        } catch (ExecutionException e) {
-            LOGGER.error("create limit fail", e);
-        }
+        RateLimiter limiter = CACHE.get(accessKeyID, new Function<String, RateLimiter>() {
+            @Override
+            public RateLimiter apply(String s) {
+                return RateLimiter.create(limit);
+            }
+        });
         if (limiter != null && !limiter.tryAcquire(LIMIT_TIME, TimeUnit.MILLISECONDS)) {
             LOGGER.error("access_key_id:{} limited", accessKeyID);
             return true;
