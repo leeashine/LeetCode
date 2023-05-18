5046bb511f039a8b6d3f6fb27d323cc8a6d7c049
lizixuan <935391770@qq.com>
2023-05-18 14:43:28 +0800

    guava限流初探

--- src/main/java/iguava/Limiter.java
+++ src/main/java/iguava/Limiter.java
@@ -1,44 +1,62 @@
 package iguava;
 
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-
 import com.google.common.base.Stopwatch;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.util.concurrent.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.*;
-import java.util.function.Function;
 
 public class Limiter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Limiter.class);
 
     private static final int CAPACITY_SIZE = 1000;
-
-    private static final long LIMIT_TIME = 1000;
-    /**
-     * qps
-     */
-    private static final double limit = 5;
-
-    private static final Cache<String, RateLimiter> CACHE = Caffeine.newBuilder().initialCapacity(CAPACITY_SIZE)
-            .expireAfterAccess(1, TimeUnit.MINUTES)
+    private static final int LIMIT_TIME = 1000;
+    private static Cache<String, RateLimiter> cache = CacheBuilder.newBuilder()
+            .initialCapacity(CAPACITY_SIZE).expireAfterAccess(1, TimeUnit.MINUTES)
             .build();
 
-    public boolean isLimit(String accessKeyID) {
-        RateLimiter limiter = CACHE.get(accessKeyID, new Function<String, RateLimiter>() {
-            @Override
-            public RateLimiter apply(String s) {
-                return RateLimiter.create(limit);
-            }
-        });
-        // tryAcquire不等待立即返回
-        if (limiter != null && !limiter.tryAcquire()) {
+    /**
+     * qps 5
+     */
+    private static double limit = 5;
+
+    static {
+        try {
+            String limitTimeStr = System
+                    .getProperty("limitTime", String.valueOf(limit));
+            limit = Double.parseDouble(limitTimeStr);
+            LOGGER.info("limitTime:{}", limit);
+        } catch (Exception e) {
+            LOGGER.error("init limitTime fail", e);
+        }
+    }
+
+    public static boolean isLimit(String accessKeyID) {
+        RateLimiter rateLimiter = null;
+        try {
+            rateLimiter = cache.get(accessKeyID, new Callable<RateLimiter>() {
+                @Override
+                public RateLimiter call() throws Exception {
+                    /**
+                     * 创建一个速率限制器（RateLimiter）的方法，可以指定一个稳定的吞吐量，以“每秒许可数”（通常称为QPS，即每秒查询数）的形式给出。
+                     * 返回的速率限制器将平均每秒发出不超过 permitsPerSecond 个许可证，并且持续的请求被平滑地分布在每一秒钟内。
+                     * 当传入请求速率超过 permitsPerSecond 时，速率限制器将每隔 (1.0 / permitsPerSecond) 秒释放一个许可证。
+                     * 当速率限制器未使用时，允许最多 permitsPerSecond 个许可证的突发，随后的请求会以 permitsPerSecond 的稳定速率进行平滑限制。
+                     * 简单来说，就是这个函数可以创建一个速率限制器对象，限制某个操作的频率，保证操作的稳定性和可靠性。
+                     */
+                    return RateLimiter.create(limit);
+                }
+            });
+        } catch (ExecutionException e) {
+            LOGGER.error("create limit fail", e);
+        }
+        if (rateLimiter != null && !rateLimiter.tryAcquire()) {
             LOGGER.error("access_key_id:{} limited", accessKeyID);
-            LOGGER.info("access_key_id:" + accessKeyID + " limited");
             return true;
         }
         return false;
@@ -47,30 +65,39 @@ public class Limiter {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         long now = System.currentTimeMillis();
         Stopwatch stopwatch = Stopwatch.createStarted();
-        Limiter limiter = new Limiter();
-        for (int i = 0; i < 20; i++) {
-
-            boolean b = limiter.isLimit("123");
-            LOGGER.info("limit:{}", b);
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+//        Limiter.isLimit("123");
+//        Thread.sleep(1000);
+        for (int i = 0; i < 10; i++) {
+            CompletableFuture.supplyAsync(() -> {
+                boolean b = Limiter.isLimit("123");
+                LOGGER.info("limit:{}", b);
+                return b;
+            }, executorService);
         }
+//        for (int i = 0; i < 20; i++) {
+//            Thread.sleep(1000);
+//            boolean b = Limiter.isLimit("123");
+//            LOGGER.info("limit:{}", b);
+//        }
 
-        stopwatch.stop();
-        LOGGER.info("cost:" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
-        Thread.sleep(500);
-        stopwatch = Stopwatch.createStarted();
-        boolean b = limiter.isLimit("123");
-        stopwatch.stop();
-        LOGGER.info("limit:{}", b);
-        LOGGER.info("cost:" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
-        Thread.sleep(500);
-        stopwatch = Stopwatch.createStarted();
-        b = limiter.isLimit("123");
-        stopwatch.stop();
-        LOGGER.info("limit:{}", b);
-        LOGGER.info("cost:" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
-
-        b = limiter.isLimit("123");
-        LOGGER.info("*********,{}", b);
+//        stopwatch.stop();
+//        LOGGER.info("cost:" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+//        Thread.sleep(500);
+//        stopwatch = Stopwatch.createStarted();
+//        boolean b = limiter.isLimit("123");
+//        stopwatch.stop();
+//        LOGGER.info("limit:{}", b);
+//        LOGGER.info("cost:" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+//        Thread.sleep(500);
+//        stopwatch = Stopwatch.createStarted();
+//        b = limiter.isLimit("123");
+//        stopwatch.stop();
+//        LOGGER.info("limit:{}", b);
+//        LOGGER.info("cost:" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+//
+//        b = limiter.isLimit("123");
+//        LOGGER.info("*********,{}", b);
 
     }
 
--- src/main/java/juc/disruptor/miaosha/EventHandler.java
+++ src/main/java/juc/disruptor/miaosha/EventHandler.java
@@ -1,5 +1,7 @@
 package juc.disruptor.miaosha;
 
+import juc.disruptor.EventQueue;
+
 public class EventHandler {
     public void onEvent(String skuId, String eventType, EventQueue queue) {
 
