5aa85b99b08ba5127a326c84283b25aa54ef724f
lizixuan <935391770@qq.com>
2024-01-26 11:13:01 +0800

    动态topK问题

--- src/main/java/aleetcode/sort/DynamicMinKElements.java
+++ src/main/java/aleetcode/sort/DynamicMinKElements.java
@@ -6,31 +6,43 @@ import java.util.PriorityQueue;
  * 利用堆动态求topK
  */
 public class DynamicMinKElements {
-    private PriorityQueue<Integer> minHeap;
+
+    static class Station {
+        int level;
+        String name;
+
+        Double price;
+
+        public Station(Double price) {
+            this.price = price;
+        }
+    }
+
+    private PriorityQueue<Station> minHeap;
     private int k;
 
     public DynamicMinKElements(int k) {
         this.k = k;
-        minHeap = new PriorityQueue<>((a, b) -> b - a); // 创建大小为k的最大堆
+        minHeap = new PriorityQueue<>((a, b) -> b.price.compareTo(a.price)); // 创建大小为k的最大堆
     }
 
-    public void add(int num) {
+    public void add(Station station) {
         // 如果堆的大小小于k，直接添加元素
         if (minHeap.size() < k) {
-            minHeap.offer(num);
+            minHeap.offer(station);
         } else {
             // 如果堆已满，并且新元素小于堆顶元素，替换堆顶元素
-            if (num < minHeap.peek()) {
+            if (station.price.compareTo(minHeap.peek().price) < 0) {
                 minHeap.poll(); // 移除堆顶元素
-                minHeap.offer(num); // 添加新元素
+                minHeap.offer(station); // 添加新元素
             }
         }
     }
 
-    public int[] getMinimumK() {
-        int[] result = new int[k];
+    public Station[] getMinimumK() {
+        Station[] result = new Station[k];
         int index = 0;
-        for (int num : minHeap) {
+        for (Station num : minHeap) {
             result[index++] = num;
         }
         return result;
@@ -38,18 +50,19 @@ public class DynamicMinKElements {
 
     public static void main(String[] args) {
         DynamicMinKElements solution = new DynamicMinKElements(3); // 求最小的3个值
-        solution.add(4);
-        solution.add(2);
-        solution.add(7);
-        solution.add(1);
-        solution.add(9);
-        solution.add(3);
-        solution.add(6);
+
+        solution.add(new Station(4.0));
+        solution.add(new Station(2.0));
+        solution.add(new Station(7.0));
+        solution.add(new Station(1.0));
+        solution.add(new Station(9.0));
+        solution.add(new Station(3.0));
+        solution.add(new Station(6.0));
 
         System.out.println("最小的3个值为:");
-        int[] result = solution.getMinimumK();
-        for (int num : result) {
-            System.out.print(num + " ");
+        Station[] result = solution.getMinimumK();
+        for (Station num : result) {
+            System.out.print(num.price + " ");
         }
     }
 }
