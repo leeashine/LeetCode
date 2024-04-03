c3c52f663e7f32237be5eddb7e4c887fa24151e3
leeshine <935391770@qq.com>
2024-04-03 13:50:00 +0800

    LeetCode100题-day03-两数之和

--- src/main/java/aleetcode/L100Problem.java
+++ src/main/java/aleetcode/L100Problem.java
@@ -5,17 +5,76 @@ import java.util.*;
 public class L100Problem {
 
     public static void main(String[] args) {
-        System.out.println('t'+'i');
-        System.out.println('h'+'u');
 
-        System.out.println((char)(1+'a'));
+        L100Problem problem = new L100Problem();
 
-        String[] strs = new String[]{"ddddddddddg","dgggggggggg"};
-        L100Problem s = new L100Problem();
-//        List<List<String>> lists = s.groupAnagrams(strs);
-        List<List<String>> lists2 = s.groupAnagrams2(strs);
-//        System.out.println(lists);
-        System.out.println(lists2);
+//        String[] strs = new String[]{"ddddddddddg","dgggggggggg"};
+////        List<List<String>> lists = s.groupAnagrams(strs);
+//        List<List<String>> lists2 = s.groupAnagrams2(strs);
+//        System.out.println(lists2);
+
+        int[] nums = new int[]{3,3};
+        int[] ints = problem.twoSum2(nums, 6);
+        System.out.println(Arrays.toString(ints));
+    }
+
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那两个整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     * 你可以按任意顺序返回答案。
+     *
+     * 同一个位置不能重复
+     *
+     * 示例 1：
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * 示例 2：
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     * 示例 3：
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     *
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            List<Integer> poList = map.getOrDefault(num, new ArrayList<>());
+            poList.add(i);
+            map.put(num, poList);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            List<Integer> left = map.get(target - num);
+            if (left != null && !left.isEmpty()) {
+                result[0] = i;
+                for (Integer j : left) {
+                    if (j != i) {
+                        result[1] = j;
+                        // 2层循环 需要跳过
+                        return result;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int rest = target - nums[i];
+            if (map.containsKey(rest)) {
+                return new int[]{map.get(rest), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 
     /**
