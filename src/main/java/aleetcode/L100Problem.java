42109ad426ed618f54e770247ed371be0af77652
leeshine <935391770@qq.com>
2024-04-07 11:08:00 +0800

    LeetCode100题-day04-移动零

--- src/main/java/aleetcode/L100Problem.java
+++ src/main/java/aleetcode/L100Problem.java
@@ -13,19 +13,94 @@ public class L100Problem {
 //        List<List<String>> lists2 = s.groupAnagrams2(strs);
 //        System.out.println(lists2);
 
-        int[] nums = new int[]{3,3};
-        int[] ints = problem.twoSum2(nums, 6);
-        System.out.println(Arrays.toString(ints));
+//        int[] nums = new int[]{3,3};
+//        int[] ints = problem.twoSum2(nums, 6);
+//        System.out.println(Arrays.toString(ints));
+
+
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        problem.moveZeroes2(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * 双指针1
+     * 283. 移动零
+     * 简单
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * <p>
+     * 示例 1:
+     * 输入: nums = [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * 示例 2:
+     * 输入: nums = [0]
+     * 输出: [0]
+     * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+     * 进阶：你能尽量减少完成的操作次数吗？
+     */
+    public void moveZeroes(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int n = nums[i];
+            int j = i + 1;
+            while (j < nums.length) {
+                // 当遇到第一个不等于0的数，交换位置
+                if (nums[j] != 0 && n == 0) {
+                    nums[i] = nums[j];
+                    nums[j] = n;
+                    break;
+                }
+                j++;
+            }
+        }
+
+
     }
 
 
     /**
+     * 使用双指针，左指针指向当前已经处理好的序列的尾部，右指针指向待处理序列的头部。
+     * <p>
+     * 右指针不断向右移动，每次右指针指向非零数，则将左右指针对应的数交换，同时左指针右移。
+     * <p>
+     * 注意到以下性质：
+     * <p>
+     * 左指针左边均为非零数；
+     * <p>
+     * 右指针左边直到左指针处均为零。
+     * <p>
+     * 因此每次交换，都是将左指针的零与右指针的非零数交换，且非零数的相对顺序并未改变。
+     * <p>
+     * 作者：力扣官方题解
+     * 链接：https://leetcode.cn/problems/move-zeroes/solutions/489622/yi-dong-ling-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     */
+    public void moveZeroes2(int[] nums) {
+        int left = 0, right = 0;
+        int n = nums.length;
+        while (right < n) {
+            if (nums[right] != 0) {
+                int t = nums[left];
+                nums[left] = nums[right];
+                nums[right] = t;
+                ++left;
+            }
+            ++right;
+        }
+    }
+
+
+    /**
+     * 哈希1
      * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那两个整数，并返回它们的数组下标。
      * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
      * 你可以按任意顺序返回答案。
-     *
+     * <p>
      * 同一个位置不能重复
-     *
+     * <p>
      * 示例 1：
      * 输入：nums = [2,7,11,15], target = 9
      * 输出：[0,1]
@@ -36,7 +111,6 @@ public class L100Problem {
      * 示例 3：
      * 输入：nums = [3,3], target = 6
      * 输出：[0,1]
-     *
      */
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -78,18 +152,20 @@ public class L100Problem {
     }
 
     /**
+     * 哈希2
      * 128. 最长连续序列
-     *     给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+     * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+     * <p>
+     * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+     * <p>
+     * 示例 1：
+     * 输入：nums = [100,4,200,1,3,2]
+     * 输出：4
+     * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+     * 示例 2：
+     * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+     * 输出：9
      *
-     *     请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
-     *
-     *     示例 1：
-     *     输入：nums = [100,4,200,1,3,2]
-     *     输出：4
-     *     解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
-     *     示例 2：
-     *     输入：nums = [0,3,7,2,5,8,4,6,0,1]
-     *     输出：9
      * @param nums
      * @return
      */
@@ -142,33 +218,35 @@ public class L100Problem {
     }
 
     /**
+     * 哈希3
      * 49. 字母异位词分组
      * 中等
      * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
-     *
+     * <p>
      * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
      * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
      * 示例 2:
-     *
+     * <p>
      * 输入: strs = [""]
      * 输出: [[""]]
      * 示例 3:
-     *
+     * <p>
      * 输入: strs = ["a"]
      * 输出: [["a"]]
-     *
-     *
+     * <p>
+     * <p>
      * 提示：
-     *
+     * <p>
      * 1 <= strs.length <= 104
      * 0 <= strs[i].length <= 100
      * strs[i] 仅包含小写字母
+     *
      * @param strs
      * @return
      */
